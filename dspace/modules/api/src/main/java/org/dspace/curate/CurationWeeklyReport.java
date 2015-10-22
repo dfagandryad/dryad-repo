bbe77815ca0d34a33dd1b42ad050aa73cbc5d29b
dfagandryad <dfagan@datadryad.org>
2015-10-22 13:51:59 -0400

    add user input

--- dspace/modules/api/src/main/java/org/dspace/curate/CurationWeeklyReport.java
+++ dspace/modules/api/src/main/java/org/dspace/curate/CurationWeeklyReport.java
@@ -398,6 +398,88 @@ public class CurationWeeklyReport extends AbstractCurationTask {
 	return Curator.CURATE_SUCCESS;
     }
 
+
+
+    /**
+       Date validation using SimpleDateFormat
+       Takes a string and verifies it is a legal date in the expected format
+    **/
+
+	public boolean isValidDate(String date)
+	{
+		// set date format, this can be changed to whatever format
+		// you want, MM-dd-yyyy, MM.dd.yyyy, dd.MM.yyyy etc.
+		// you can read more about it here:
+		// http://java.sun.com/j2se/1.4.2/docs/api/index.html
+
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyyddMM");
+
+		// declare and initialize testDate variable, this is what will hold
+		// our converted string
+
+		Date testDate = null;
+
+		// we will now try to parse the string into date form
+		try
+		{
+		  testDate = sdf.parse(date);
+		}
+
+		// if the format of the string provided doesn't match the format we 
+		// declared in SimpleDateFormat() we will get an exception
+
+		catch (ParseException e)
+		{
+			errorMessage = "the date you provided is in an invalid date" + " format.";
+			return false;
+		}
+
+		// dateformat.parse will accept any date as long as it's in the format
+		// you defined, it simply rolls dates over, for example, december 32 
+		// becomes jan 1 and december 0 becomes november 30
+		// This statement will make sure that once the string 
+		// has been checked for proper formatting that the date is still the 
+		// date that was entered, if it's not, we assume that the date is invalid
+
+		if (!sdf.format(testDate).equals(date)) 
+		{
+			errorMessage = "The date that you provided is invalid.";
+			return false;
+		}
+
+		// if we make it to here without getting an error it is assumed that
+		// the date was a valid one and that it's in the proper format
+
+		return true;
+
+	} // end isValidDate
+
+
+    /**
+       Get user input for beginning date.
+    **/
+    
+	public string userInputDate(String prompt)
+	{
+    	Scanner inputReg = new Scanner(System.in);
+    	Date theDate =  = "99990101;    
+ 
+	    System.out.print(prompt);
+    	try {
+        	theDate = new SimpleDateFormat("yyyyddMM").parse(inputReg.nextLine().replaceAll("/", ""));
+    	} catch (ParseException e) {
+        	e.printStackTrace();
+	    }
+    	inputReg.close();
+    	if isValidDate(theDate) {    
+			return theDate;
+		} else
+			return theDate;
+			
+	} // end userInputDate
+
+
+
     /**
        An XML utility method that returns the text content of a node.
     **/
